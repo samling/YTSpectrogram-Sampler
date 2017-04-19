6e3b358cadd6279cc902a00696962b765ddc6adb
samling <samlingx@gmail.com>
2017-04-18 20:07:24 -0700

    Much more sensible JSON marshaling, better variable names

--- src/YTSpectrogram/ytspectrogram.go
+++ src/YTSpectrogram/ytspectrogram.go
@@ -5,22 +5,36 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
+	_ "github.com/jmoiron/sqlx"
 	_ "github.com/kennygrant/sanitize"
 	"github.com/mdlayher/waveform"
 	"io"
+	"log"
 	"math"
 	"os"
 )
+
+type Data struct {
+	Samples []Sample
+}
+
+type Sample struct {
+	Timestamp  int
+	SampleData float64
+}
 
 // Piggybacking off the waveform library to retrieve just the sample values instead of an image
 func GetSampleValues(r io.Reader, options ...waveform.OptionsFunc) ([]float64, error) {
 	w, err := waveform.New(r, options...)
 	if err != nil {
-		return nil, err
+		log.Fatal("Unable to generate waveform data: ", err)
 	}
 
 	values, err := w.Compute()
+	if err != nil {
+		log.Fatal("Unable to compute sample data: ", err)
+	}
+
 	return values, nil
 }
 
@@ -40,14 +54,13 @@ func Round(val float64, roundOn float64, places int) (newVal float64) {
 	return
 }
 
+// Formulate a connection string from environment variables
 func GetConnectionString(dbHost string, dbUser string, dbPass string, dbName string) string {
 	return fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPass, dbHost, dbName)
 }
 
 func main() {
-	// Protect against path traversal via injection
-	// TODO: Resolve conflicts with URLs that have "_" in them
-	//id := sanitize.Path(os.Args[1])
+	// Get the ID of our video from the docker run argument
 	id := os.Args[1]
 
 	// Open an IO Reader for our FLAC file
@@ -57,50 +70,51 @@ func main() {
 	}
 	defer r.Close()
 
-	// Set some basic values
-	// max = Tracks the maximum sample value
-	// resolution = Specifies the sample rate per second (default 4)
-	// m = Map of quarter second to value
-	max := 0
-	resolution := uint(4)
-	m := make(map[int]interface{})
+	// Initialize some variables
+	var (
+		max  = 0
+		data Data
+	)
 
-	// Get sample values with our default values
-	values, err := GetSampleValues(r,
+	// Get sample data with our default values
+	sampleValues, err := GetSampleValues(r,
 		nil,
 		nil,
-		waveform.Resolution(resolution),
+		waveform.Resolution(4),
 		nil,
 		waveform.ScaleClipping(),
 		waveform.Sharpness(1),
 	)
 
-	// Determine the maximum value
-	for _, f := range values {
-		max = int(math.Max(float64(max), Round((f*1E6), .5, 0)))
+	// Determine the maximum amplitude value
+	for _, val := range sampleValues {
+		max = int(math.Max(float64(max), Round((val*1E6), .5, 0)))
 	}
 
-	// Adjust our values to be percentages of the max
-	for t, f := range values {
-		adjusted := Round((f*1E6), .5, 0) / float64(max)
-		m[t] = adjusted
+	// Adjust our values to be percentages of the max and add them to our data struct
+	for time, val := range sampleValues {
+		adjustedVal := Round((val*1E6), .5, 0) / float64(max)
+		data.Samples = append(data.Samples, Sample{time, adjustedVal})
 	}
 
 	// TODO: Average our values to smooth out any blips and outliers
 	// TODO: Identify them first?
 
 	// Serialize our map as a JSON dict
-	jsonData, _ := json.Marshal(m)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal("Cannot encode to JSON: ", err)
+	}
 
 	// Connect to our database
-	connString := GetConnectionString(os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
-	db := sqlx.MustConnect("mysql", connString)
+	//connString := GetConnectionString(os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+	//db := sqlx.MustConnect("mysql", connString)
 
-	// Write id and sampledata to db, ignore duplicates
-	tx := db.MustBegin()
-	tx.MustExec("INSERT INTO Sample (Id, SampleData) VALUES (?, ?) ON DUPLICATE KEY UPDATE Id=Id", id, jsonData)
-	tx.Commit()
+	//// Write id and sampledata to db, ignore duplicates
+	//tx := db.MustBegin()
+	//tx.MustExec("INSERT INTO Sample (Id, SampleData) VALUES (?, ?) ON DUPLICATE KEY UPDATE Id=Id", id, jsonData)
+	//tx.Commit()
 
 	// Print the map
-	//fmt.Println(string(jsonData))
+	fmt.Println(string(jsonData))
 }
